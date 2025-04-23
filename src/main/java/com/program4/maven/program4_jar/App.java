bd8e211c3b15a48431294fb2c12c84a98a683a39
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-04-23 10:36:59 +0530

    Asure pipeline setup

---
+++ src/main/java/com/program4/maven/program4_jar/App.java
@@ -0,0 +1,13 @@
+package com.program4.maven.program4_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Program4!" );
+    }
+}
